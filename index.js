d20b78b295aac146e547cb0cd1ec7f9a068ec78c
vincent44-art <vincent.murithi@student.moringaschoo.com>
2025-03-11 12:14:38 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+// Declare customerName in global scope
+var customerName = 'bob';
+
+// Function to uppercase customerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Function to set bestCustomer
+function setBestCustomer() {
+  bestCustomer = 'not bob'; // Declares a global variable without var, let, or const
+}
+
+// Function to overwrite bestCustomer
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// Declare leastFavoriteCustomer as a constant
+const leastFavoriteCustomer = 'someone';
+
+// Function attempting to change leastFavoriteCustomer (will cause an error)
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'another person'; // This will cause an error
+}
